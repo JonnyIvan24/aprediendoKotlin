e431fc4261d742f8abac3f3e8c3b791917135152
JonnyIvan24 <johnny.perez.u@gmail.com>
2024-09-28 12:46:07 -0600

    Strings y sus métodos

--- app/src/main/java/com/example/myfirstapplication/Main.kt
+++ app/src/main/java/com/example/myfirstapplication/Main.kt
@@ -14,10 +14,30 @@ fun main() {
     println("tengo ${clientes} clientes")
     println("tengo ${1} clientes")
 
+    var miTexto2: String
 
+//    println(miTexto2) // nos da error por que no hay un valor iniciado en la variable
 
+    // multi-línea
 
+    val miMultilinea = """
+        primera línea
+        segunda línea
+    """
+    miTexto2 = " mundo"
+    println(miMultilinea.trimIndent())
+    println(miTexto.length)
+    println(miTexto + miTexto2)
+    var miPrimerCaracter = miTexto[0] // siempre nos dara un valor de tipo Char
+//    miTexto2 = miPrimerCaracter nos da error por que no es un valor de tipo string
+    miTexto2 = miPrimerCaracter.toString()
+    println(miTexto[0])
+    println( miTexto[miTexto.length - 1])
 
+    // String.format acepta dos argumentos
+    val numeroEntero = String.format("%07d", 31416)
+    println(numeroEntero)
 
-
+    val holaMundo = String.format("%S %S", "hola", "mundo")
+    println(holaMundo)
 }
