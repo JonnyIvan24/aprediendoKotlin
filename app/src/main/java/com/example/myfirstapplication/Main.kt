9a01d0d6abf7266306fa528d1fa8d76e5462eee3
JonnyIvan24 <johnny.perez.u@gmail.com>
2024-09-28 11:35:29 -0600

    Hola mundo

---
+++ app/src/main/java/com/example/myfirstapplication/Main.kt
@@ -0,0 +1,23 @@
+package com.example.myfirstapplication
+
+fun main() {
+    var miTexto = "hola mundo"
+    miTexto = "hola"
+
+    val miTextoInmutable = "ya no se cambia este valor"
+
+    println(miTexto)
+
+    val clientes = 10
+
+    // String templates
+    println("tengo ${clientes} clientes")
+    println("tengo ${1} clientes")
+
+
+
+
+
+
+
+}
