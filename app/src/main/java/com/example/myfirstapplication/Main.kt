9b31ef522e66c46e83f1e5f5c47c8eaa74a06a2e
JonnyIvan24 <johnny.perez.u@gmail.com>
2024-10-05 10:25:52 -0600

    Números y sus métodos

--- app/src/main/java/com/example/myfirstapplication/Main.kt
+++ app/src/main/java/com/example/myfirstapplication/Main.kt
@@ -14,6 +14,12 @@ fun main() {
     println("tengo ${clientes} clientes")
     println("tengo ${1} clientes")
 
+    // strings()
+    enteros()
+}
+
+fun strings() {
+    val miTexto = "hola mundo"
     var miTexto2: String
 
 //    println(miTexto2) // nos da error por que no hay un valor iniciado en la variable
@@ -40,4 +46,32 @@ fun main() {
 
     val holaMundo = String.format("%S %S", "hola", "mundo")
     println(holaMundo)
+}
+
+fun enteros() {
+    val myByte: Byte = 123
+    val miEntero: Int = myByte.toInt()
+    // println(myByte == miEntero) // no podemos comparar diferentes tipos de valores
+
+    // para declarar una flotante como un long es necesario especificar el tipo en el valor
+    val miFlotante = 1.5F
+    println(miFlotante)
+
+    val b: Float
+
+    // oparedores aritmeticos
+
+    val x = 5 / 2 // una división de enteros siempre retorna un entero
+    println(x == 2)
+
+    val y = 5.toDouble() / 2 // si uno de los valores es de tipo flotante se retorna ese mismo tipo
+    println(y == 2.5)
+
+    // constantes literales
+    val cienMil = 100_000
+    println(cienMil)
+    val miHexa = 0xF
+    println(miHexa + 10)
+
+    println(12.5e10) // 0.000000125
 }
